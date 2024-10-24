450c8e3d56af4df24b7f8d5650907d1962cb7cff
Juan Manuel Bolivar <jbolivarm2@ucentral.edu.co>
2024-10-24 13:25:07 -0500

    Creacion de usuario

--- app/src/main/java/com/example/aplicativomovil/registrarse.java
+++ app/src/main/java/com/example/aplicativomovil/registrarse.java
@@ -1,5 +1,7 @@
 package com.example.aplicativomovil;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +16,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.aplicativomovil.DataBase.DataBaseUsuario;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.FirebaseAuthCredentialsProvider;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,11 +33,14 @@ import java.util.Map;
 public class registrarse extends AppCompatActivity  implements View.OnClickListener{
 
     private EditText txtnombre,txttelefono,txtcorreo_electronico;
-
+    private EditText txtcontrasena,txtconfirmarcontrasena;
     private Button btnguardar;
 
     //VARIABLE FIREBASE
     private FirebaseFirestore db;
+    //AUTENTIFICACION CUENTA
+    @SuppressLint("RestrictedApi")
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,40 +55,83 @@ public class registrarse extends AppCompatActivity  implements View.OnClickListe
 
         //INICIALIZAR FIREBASE
         db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         // TEXTO
         txtnombre = findViewById(R.id.txtNombre);
         txttelefono = findViewById(R.id.txtTelefono);
         txtcorreo_electronico = findViewById(R.id.txtCorreo);
+        //CONTRASENA
+        txtcontrasena = findViewById(R.id.txtcontrasena);
+        txtconfirmarcontrasena = findViewById(R.id.txtConfirmarContrasena);
+
         // BOTON
         btnguardar = findViewById(R.id.btnGuardar);
 
-        //CREAR ACTION LISTENER
+        //CREAR ACTION LISTENER PARA EL BOTON
         btnguardar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        //CONVERTIR VALORES A STRING DE EDIT TEXT
         String nombre = txtnombre.getText().toString().trim();
         String telefono = txttelefono.getText().toString().trim();
         String correo_electronico = txtcorreo_electronico.getText().toString().trim();
+        String contrasena = txtcontrasena.getText().toString().trim();
+        String confirContrasena = txtconfirmarcontrasena.getText().toString().trim();
 
 
-
-        if(view.getId() == R.id.btnGuardar){
+        if(view.getId() == R.id.btnGuardar){//ESCUCHAR EL EVENTO BOTON GUARDAR
+            //REVISAR QUE LOS EDIT TEXT NO ESTEN VACIOS
+            
             if(nombre.isEmpty() && telefono.isEmpty() && correo_electronico.isEmpty()){
                 Toast.makeText(this, "por favor,ingrese todos  los datos ", Toast.LENGTH_SHORT).show();
 
+
             }else{
-                postUsuario(nombre,telefono,correo_electronico);
-                limpiarCampos();
+                if(!correo_electronico.isEmpty() ){
+                    if( contrasena.equals(confirContrasena)){
+                        Toast.makeText(this, "Contraseña coincide ", Toast.LENGTH_SHORT).show();
+                        crearUsuario(correo_electronico, contrasena);
+                        limpiarCampos();//LIMPIAR CAMPOS DEL EDIT TEXT
+                    }else {
+                        Toast.makeText(registrarse.this, "Contraseña no Coincide", Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    Toast.makeText(registrarse.this, "Por favor ingrese Correo Electronico", Toast.LENGTH_LONG).show();
+                }
             }
 
         }
     }
+    private void crearUsuario(String correo, String cont){
+        mAuth.createUserWithEmailAndPassword(correo, cont)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(registrarse.this,"Bienvenido ", Toast.LENGTH_LONG).show();
+                            // Ahora que el usuario fue creado, guarda sus datos en Firestore
+                            postUsuario(txtnombre.getText().toString().trim(), txttelefono.getText().toString().trim(), correo);
+                            Intent intent = new Intent(registrarse.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(registrarse.this, "Error: no se pudo crear el usuario", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(registrarse.this,"Ha ocurrido un error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
 
     private void postUsuario(String nombre, String telefono, String correoElectronico) {
+        //FUNCION PARA GUARDAR LOS VALORES  A LA BASE DE DATOS
+
         Map<String, Object> user = new HashMap<>();
         //AGREGAR VALORES AL MAPA CLAVE-VALOR
         user.put("Nombre",nombre);
