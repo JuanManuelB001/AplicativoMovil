388d5d3b0b863375302f967b1ca1dc71c9f14c00
Juan Manuel Bolivar <jbolivarm2@ucentral.edu.co>
2024-10-25 21:21:55 -0500

    Arreglo de import registro.java

--- app/src/main/java/com/example/aplicativomovil/registrarse.java
+++ app/src/main/java/com/example/aplicativomovil/registrarse.java
@@ -22,25 +22,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class registrarse extends AppCompatActivity  implements View.OnClickListener{
+public class registrarse extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText txtnombre,txttelefono,txtcorreo_electronico;
-    private EditText txtcontrasena,txtconfirmarcontrasena;
+    private EditText txtnombre, txttelefono, txtcorreo_electronico;
+    private EditText txtcontrasena, txtconfirmarcontrasena;
     private Button btnguardar;
-    private EditText txtnom;
-    public  long maxid=0;
-
 
     //VARIABLE FIREBASE
     private FirebaseFirestore db;
@@ -59,7 +51,7 @@ public class registrarse extends AppCompatActivity  implements View.OnClickListe
             return insets;
         });
 
-        //INICIALIZAR FIREBASE
+        // INICIALIZAR FIREBASE
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
@@ -67,112 +59,103 @@ public class registrarse extends AppCompatActivity  implements View.OnClickListe
         txtnombre = findViewById(R.id.txtNombreUsuario);
         txttelefono = findViewById(R.id.txtTelefono);
         txtcorreo_electronico = findViewById(R.id.txtCorreo);
-        //CONTRASENA
         txtcontrasena = findViewById(R.id.txtcontrasena);
         txtconfirmarcontrasena = findViewById(R.id.txtConfirmarContrasena);
 
-
         // BOTON
         btnguardar = findViewById(R.id.btnGuardar);
-        Button btnsiguiente;
-        btnsiguiente = findViewById(R.id.btnSiguiente);
+        Button btnsiguiente = findViewById(R.id.btnSiguiente);
 
-        //CREAR ACTION LISTENER PARA EL BOTON
+        // CREAR ACTION LISTENER PARA EL BOTON
         btnguardar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        //CONVERTIR VALORES A STRING DE EDIT TEXT
+        // CONVERTIR VALORES A STRING DE EDIT TEXT
         String nombre = txtnombre.getText().toString().trim();
         String telefono = txttelefono.getText().toString().trim();
         String correo_electronico = txtcorreo_electronico.getText().toString().trim();
         String contrasena = txtcontrasena.getText().toString().trim();
         String confirContrasena = txtconfirmarcontrasena.getText().toString().trim();
 
-        if(view.getId() == R.id.btnSiguiente){
+        if (view.getId() == R.id.btnSiguiente) {
             Intent intent = new Intent();
+            // Agrega la lógica para la siguiente actividad aquí
         }
 
-        if(view.getId() == R.id.btnGuardar){//ESCUCHAR EL EVENTO BOTON GUARDAR
-            //REVISAR QUE LOS EDIT TEXT NO ESTEN VACIOS
-            
-            if(nombre.isEmpty() && telefono.isEmpty() && correo_electronico.isEmpty()){
-                Toast.makeText(this, "por favor,ingrese todos  los datos ", Toast.LENGTH_SHORT).show();
-
-
-            }else{
-                if(!correo_electronico.isEmpty() ){
-                    if( contrasena.equals(confirContrasena)){
-                        Toast.makeText(this, "Contraseña coincide ", Toast.LENGTH_SHORT).show();
+        if (view.getId() == R.id.btnGuardar) { // ESCUCHAR EL EVENTO BOTON GUARDAR
+            // REVISAR QUE LOS EDIT TEXT NO ESTEN VACIOS
+            if (nombre.isEmpty() && telefono.isEmpty() && correo_electronico.isEmpty()) {
+                Toast.makeText(this, "Por favor, ingrese todos los datos", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!correo_electronico.isEmpty()) {
+                    if (contrasena.equals(confirContrasena)) {
+                        Toast.makeText(this, "Contraseña coincide", Toast.LENGTH_SHORT).show();
                         crearUsuario(correo_electronico, contrasena);
-                    }else {
+                    } else {
                         Toast.makeText(registrarse.this, "Contraseña no Coincide", Toast.LENGTH_LONG).show();
                     }
-                }else {
-                    Toast.makeText(registrarse.this, "Por favor ingrese Correo Electronico", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(registrarse.this, "Por favor ingrese Correo Electrónico", Toast.LENGTH_LONG).show();
                 }
             }
-
         }
     }
-    private void crearUsuario(String correo, String cont){
+
+    private void crearUsuario(String correo, String cont) {
         mAuth.createUserWithEmailAndPassword(correo, cont)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(registrarse.this,"Registro Exitoso ", Toast.LENGTH_LONG).show();
+                            Toast.makeText(registrarse.this, "Registro Exitoso", Toast.LENGTH_LONG).show();
                             // Ahora que el usuario fue creado, guarda sus datos en Firestore
                             postUsuario(txtnombre.getText().toString().trim(), txttelefono.getText().toString().trim(), correo);
                             Intent intent = new Intent(registrarse.this, MainActivity.class);
                             startActivity(intent);
                         } else {
-                            if(cont.length() < 6){
-                                Toast.makeText(registrarse.this, "Error:Ingrese una contraseña valida", Toast.LENGTH_LONG).show();
-                                Toast.makeText(registrarse.this,"Minimo 6 Caracteres.",Toast.LENGTH_LONG).show();
+                            if (cont.length() < 6) {
+                                Toast.makeText(registrarse.this, "Error: Ingrese una contraseña válida", Toast.LENGTH_LONG).show();
+                                Toast.makeText(registrarse.this, "Mínimo 6 Caracteres.", Toast.LENGTH_LONG).show();
                             }
-
                         }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(registrarse.this,"Ha ocurrido un error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(registrarse.this, "Ha ocurrido un error: " + e.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
     private void postUsuario(String nombre, String telefono, String correoElectronico) {
-        //FUNCION PARA GUARDAR LOS VALORES  A LA BASE DE DATOS
-
+        // FUNCION PARA GUARDAR LOS VALORES A LA BASE DE DATOS
         Map<String, Object> user = new HashMap<>();
-        //AGREGAR VALORES AL MAPA CLAVE-VALOR
-        user.put("Nombre",nombre);
+        // AGREGAR VALORES AL MAPA CLAVE-VALOR
+        user.put("Nombre", nombre);
         user.put("Telefono", telefono);
-        user.put("Correo Electronico",correoElectronico);
+        user.put("Correo Electronico", correoElectronico);
 
-        //AGREGAR VALORES A LA BASE DE DATOS
+        // AGREGAR VALORES A LA BASE DE DATOS
         db.collection("Usuarios")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(registrarse.this, "Exito al guardar en la base de datos",Toast.LENGTH_LONG).show();
+                        Toast.makeText(registrarse.this, "Éxito al guardar en la base de datos", Toast.LENGTH_LONG).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Toast.makeText(registrarse.this, "Fallo al guardar", Toast.LENGTH_SHORT).show();
-
                     }
                 });
-
     }
 
-    private void limpiarCampos(){
+    private void limpiarCampos() {
         txtnombre.setText("");
         txttelefono.setText("");
         txtcorreo_electronico.setText("");
